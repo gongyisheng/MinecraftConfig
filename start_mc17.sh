5d0a53926509bbbde462a60d6029a460c9283a2a
gongyisheng <temp@Melissas-MacBook-Pro.local>
2022-07-25 00:54:24 -0400

    update start script

--- start_mc17.sh
+++ start_mc17.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /root/opt/MC1.17
-nohup java -Xmx1024M -Xms1024M -jar server.jar nogui >/root/MC1.17.log &
+#Xms=min_heap_size, Xmx=max_heap_size
+cd /root/MC1.17
+nohup java -Xms1024M -Xmx2048M -jar server.jar nogui >/root/MC1.17.log &
 tail -f /root/MC1.17.log
