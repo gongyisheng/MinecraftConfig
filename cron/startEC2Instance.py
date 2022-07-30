24bdf60ac5c2fa13e51bd5a1e849d9a78e061890
gongyisheng <temp@Melissas-MacBook-Pro.local>
2022-07-30 00:51:03 -0400

    add start and stop EC2 instance aws lambda cron job scripts

---
+++ cron/startEC2Instance.py
@@ -0,0 +1,8 @@
+import boto3
+region = 'YOUR_EC2_INSTANCE_REGION(eg.us-east-1)'
+instances = ['YOUR_EC2_INSTANCE_ID(eg.i-12345cb6de4f78g9h)']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
