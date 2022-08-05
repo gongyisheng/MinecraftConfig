51e071c8d2b8c6fa3b7d0978c6f26eac4eb3b56e
gongyisheng <temp@Melissas-MacBook-Pro.local>
2022-08-04 22:39:18 -0400

    add comments: make sure have enough disk space before backup

--- shell/backupToLocal.sh
+++ shell/backupToLocal.sh
@@ -1,3 +1,5 @@
+# NOTE: Make sure your EC2 instance have enough disk space for backup, 
+# or you may get an error called "instances not in a valid state"
 ls_date=`date +%Y_%m_%d`
 ls_time=`date +%Y_%m_%d_%H_%M_%S`
 
