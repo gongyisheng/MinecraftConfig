00e85cb9b199ed2a6dcbcb923c9ff03f9fce48eb
Yisheng Gong <yisheng_gong@onmail.com>
2022-08-23 12:42:48 -0400

    remove dropping icon

--- monitor/get_status.py
+++ monitor/get_status.py
@@ -6,7 +6,6 @@ from config import address
 url = f"https://api.mcsrvstat.us/2/{address}"
 while True:
     resp = requests.get(url).json()
-    resp.pop("icon")
     t = time.localtime()
     t = time.strftime("%H:%M:%S", t)
     print(t, resp.get("online"))
