a7765573ca64927d0910fe129ffb6d35d09f78c1
Yisheng Gong <yisheng_gong@onmail.com>
2022-08-23 01:39:23 -0400

    add monitoring script

---
+++ monitor/get_status.py
@@ -0,0 +1,12 @@
+import requests
+import time
+
+address = ""
+url = f"https://api.mcsrvstat.us/2/{address}"
+while True:
+    resp = requests.get(url).json()
+    resp.pop("icon")
+    t = time.localtime()
+    t = time.strftime("%H:%M:%S", t)
+    print(t, resp.get("online"))
+    time.sleep(60)
